762b9ec75cf77eda7edd7e5600b6143ba248bdf6
AlexNiko01 <nikonenkoaleksandra@gmail.com>
2016-09-03 03:35:06 +0300

    scripts

---
+++ search2.php
@@ -0,0 +1,100 @@
+<?php include 'header.php' ?>
+<main class="search">
+    <div class="row">
+        <div class="column">
+            <header class="search__header">
+                <h1 class="search__header-title">Searching results</h1>
+                <h3 class="search__result-title">HASBRO</h3>
+            </header>
+            <div class="search__content">
+                <div class="search-form-parent">
+                    <form method="get" id="searchform" action="">
+                        <div class="row collapse">
+                            <div class="small-10 columns">
+                                <input type="search" name="search" id="search" placeholder="Search on ZebraWorldWide">
+                            </div>
+                            <div class="small-2 columns">
+                                <input type="submit" name="s" id="searchsubmit" value="Search">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="search-results">
+        <div class="grid">
+            <a href="#" class="col-37 tiles__column tiles__item wow fadeInUp">
+                <div class="tiles__img" style="background-image: url('img/23.png')"></div>
+                <div class="tiles__hover" style="background-color:rgba(224,224,224,0.7)">
+                    <div class="tiles__hover-content">
+                        <h4 class="tiles__post-title">PLI</h4>
+                        <span>Liptons Hike</span>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="col-26 tiles__column tiles__item wow fadeInUp">
+
+                <div class="tiles__img" style="background-image: url('img/25.png')"></div>
+                <div class="tiles__hover" style="background-color:rgba(224,224,224,0.7)">
+                    <div class="tiles__hover-content">
+                        <h4 class="tiles__post-title">PLI</h4>
+                        <span>Liptons Hike</span>
+                    </div>
+                </div>
+
+            </a>
+
+            <a href="#" class="col-37 tiles__column tiles__item wow fadeInUp">
+                <div class="tiles__img" style="background-image: url('img/24.png')"></div>
+                <div class="tiles__hover" style="background-color:rgba(224,224,224,0.7)">
+                    <div class="tiles__hover-content">
+                        <h4 class="tiles__post-title">PLI</h4>
+                        <span>Liptons Hike</span>
+                    </div>
+                </div>
+
+            </a>
+            <a href="#" class="col-26 tiles__column tiles__item wow fadeInUp">
+
+                <div class="tiles__img" style="background-image: url('img/25.png')"></div>
+                <div class="tiles__hover" style="background-color:rgba(224,224,224,0.7)">
+                    <div class="tiles__hover-content">
+                        <h4 class="tiles__post-title">PLI</h4>
+                        <span>Liptons Hike</span>
+                    </div>
+                </div>
+
+            </a>
+            <a href="#" class="col-37 tiles__column tiles__item wow fadeInUp">
+                <div class="tiles__img" style="background-image: url('img/23.png')"></div>
+                <div class="tiles__hover" style="background-color:rgba(224,224,224,0.7)">
+                    <div class="tiles__hover-content">
+                        <h4 class="tiles__post-title">PLI</h4>
+                        <span>Liptons Hike</span>
+                    </div>
+                </div>
+            </a>
+
+
+            <a href="#" class="col-37 tiles__column tiles__item wow fadeInUp">
+                <div class="tiles__img" style="background-image: url('img/24.png')"></div>
+                <div class="tiles__hover" style="background-color:rgba(224,224,224,0.7)">
+                    <div class="tiles__hover-content">
+                        <h4 class="tiles__post-title">PLI</h4>
+                        <span>Liptons Hike</span>
+                        <span>Hike</span>
+                    </div>
+                </div>
+
+            </a>
+        </div>
+
+
+    </section>
+    <section class="discover  wow fadeInUp">
+        <a href="#" class="button">work</a>
+    </section>
+</main>
+<?php include 'footer.php' ?>
+
